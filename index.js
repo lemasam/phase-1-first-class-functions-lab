4011efd21396334154dd0eb6e9261543ad3b7514
lema samuel <lemasam10@gmail.com>
2024-04-03 13:37:02 +0300

    function-lab done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers =  function (drivers){
+    return drivers.slice(0,2);
+
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(2)
+}
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (num){
+    return function (){
+        return num*5
+    }
+}
+
+const fareDoubler = function (createFareMultiplier){
+
+    return createFareMultiplier*2
+
+}
+
+const fareTripler = function(createFareMultiplier){
+    return createFareMultiplier*3
+}
+
+const selectDifferentDrivers = function (drivers, func){
+    return func(drivers);
+
+}
